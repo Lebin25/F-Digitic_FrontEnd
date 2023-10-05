96cf1d9679344d160ed1441b364597cdc6c660fa
Quang <quang02042002@gmail.com>
2023-10-05 20:16:01 +0700

    quang/blogs Done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CompareProduct from './pages/CompareProduct';
 import Wishlist from './pages/Wishlist';
 import OurStore from './pages/OurStore';
 import SingleProduct from './pages/SingleProduct';
+import Blog from './pages/Blog';
 
 function App() {
    return <>
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Layout />}>
                <Route index element={<Home />} />
                <Route path="product" element={<OurStore />} />
+               <Route path="blogs" element={<Blog />} />
                <Route path="product/:id" element={<SingleProduct />} />
                <Route path="compare-product" element={<CompareProduct />} />
                <Route path="wishlist" element={<Wishlist />} />
