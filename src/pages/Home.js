b6f8b1ef0e379d154d3bf727fe1869ce6dafe1e6
Minh Huan <huannvmde160463@fpt.edu.vn>
2023-11-02 22:19:41 +0700

    Features/FetchFeatureProduct to Homepage Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -178,10 +178,64 @@ const Home = () => {
             <div className="col-12">
                <h3 className="section-heading">Featured Collection</h3>
             </div>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {
+               productState && productState?.map((item, index) => {
+                  if (item?.tags === 'featured') {
+                     return (
+                        <div
+                           key={index}
+                           className={"col-3"}
+                        >
+                           <Link
+                              // to={`${location.pathname == "/"
+                              //    ? '/product/:id'
+                              //    : location.pathname == '/product/:id'
+                              //       ? "/product/:id"
+                              //       : ":id"
+                              //    }`}
+                              className="product-card position-relative">
+                              <div className="wishlist-icon position-absolute">
+                                 <button className='border-0 bg-transparent' onClick={(e) => {
+                                    addProdToWishlist(item?._id)
+                                 }}>
+                                    <img src={wish} alt="wishlist" />
+                                 </button>
+                              </div>
+                              <div className="product-image">
+                                 <img src={item?.images[0]?.url} className='img-fluid mx-auto' alt="product image" width={160} />
+                                 <img src={watch2} className='img-fluid mx-auto' alt="product image" width={160} />
+                              </div>
+                              <div className="product-details">
+                                 <h6 className="brand">{item?.brand}</h6>
+                                 <h5 className="product-title">{item?.title}</h5>
+                                 <ReactStars
+                                    count={5}
+                                    size={24}
+                                    value={item?.totalrating.toString()}
+                                    edit={false}
+                                    activeColor="#ffd700"
+                                 />
+                                 <p className="price">$ {item?.price}</p>
+                              </div>
+                              <div className="action-bar position-absolute">
+                                 <div className='d-flex flex-column gap-15'>
+                                    <button className='border-0 bg-transparent'>
+                                       <img src={prodcompare} alt="compare" />
+                                    </button>
+                                    <button className='border-0 bg-transparent'>
+                                       <img src={view} alt="view" />
+                                    </button>
+                                    <button className='border-0 bg-transparent'>
+                                       <img src={addcart} alt="addcart" />
+                                    </button>
+                                 </div>
+                              </div>
+                           </Link>
+                        </div >
+                     )
+                  }
+               })
+            }
          </div>
       </Container>
 
